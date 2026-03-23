ae24d5be791fa99e9b6cc8dc1fdb7eaa3724624d
Agustin Wilson <aguswilson2015@outlook.es>
2026-03-23 00:31:24 -0300

    Actualización

--- vite.config.js
+++ vite.config.js
@@ -30,6 +30,16 @@ export default defineConfig({
 
         icons: [
           {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
             src: '/icons/icon-192-maskable.png',
             sizes: '192x192',
             type: 'image/png',
